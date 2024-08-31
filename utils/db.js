677f02fa09504bc30dfda64953e2e8c95a34c119
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-08-31 14:30:03 +0200

    Authenticate a user

--- utils/db.js
+++ utils/db.js
@@ -3,7 +3,7 @@ import { MongoClient } from 'mongodb';
 class DBClient {
   constructor() {
     const host = process.env.DB_HOST || 'localhost';
-    const port = process.env.DB_PORT || '27017';
+    const port = process.env.DB_PORT || 27017;
     const database = process.env.DB_DATABASE || 'files_manager';
     const url = `mongodb://${host}:${port}`;
 
